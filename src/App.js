5ba7ccf452483621fc54543e874bb134bf1f71be
Ashutoshubale <ashutoshubale35@gmail.com>
2022-12-30 00:17:34 +0530

    interpolation at atrribut leval

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-// user defined tags == Component
 function App () {
-
-  //pure data members
-  let project = "My React Project";
-let username = "rahul";
-
-//JSX => HTML CSS
+  let project = "Counter Application";
+  let btnvalue = "click me"
 return (
     <div>
-      <h1>Hello World</h1>
-      <h1>Hello {username} !</h1>
-      <h1>Hello {project} !</h1>
+      <h1> Counter Application Static </h1>
+      <h1> {project} </h1>
+      <input type = "button" value={btnvalue  } />
     </div>
-
-);
-
+  );
 }
 export default App;
