7edd00fc06eb0c66ecbf7d12bb98d5bf8ec58485
Ashutoshubale <ashutoshubale35@gmail.com>
2022-12-30 15:42:09 +0530

    b3 brach

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 function App () {
+
+  // Data Members
   let project = "Counter Application";
   let btnvalue = "click me"
+
+  //Member :: Type is Functions 
+  let clickMe = () => alert("ky mug kasa kay bara ahe na ");
+  let increment = () => {};
+  let decrement = () => {};
+
+  //JSX
 return (
     <div>
       <h1> Counter Application Static </h1>
       <h1> {project} </h1>
-      <input type = "button" value={btnvalue  } />
+      <input type = "button" value={btnvalue} onClick = {clickMe}/>
+      <input type="button" value="Increment" onClick={increment}/>
     </div>
   );
 }
